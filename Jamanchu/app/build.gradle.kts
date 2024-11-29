7ca3ea0510d7eea398c20495e370b2e843c59ff0
zoonmy <pjm2571@naver.com>
2024-11-29 16:24:03 +0900

    fragment

--- Jamanchu/app/build.gradle.kts
+++ Jamanchu/app/build.gradle.kts
@@ -45,10 +45,17 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     
     // 파이어베이스
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+
+    // 바텀네비게이션
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.3")
 }
+
