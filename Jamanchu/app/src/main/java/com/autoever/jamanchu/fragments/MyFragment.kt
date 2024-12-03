16cf1369fb6aa17827a33f5cb61543a1d1873ced
zoonmy <pjm2571@naver.com>
2024-12-03 10:36:33 +0900

    feat: 현대 앱

--- Jamanchu/app/src/main/java/com/autoever/jamanchu/fragments/MyFragment.kt
+++ Jamanchu/app/src/main/java/com/autoever/jamanchu/fragments/MyFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.autoever.jamanchu.R
 
 class MyFragment : Fragment() {
@@ -12,6 +13,10 @@ class MyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my, container, false)
+        val view =  inflater.inflate(R.layout.fragment_my, container, false)
+
+        val textViewLogout = view.findViewById<TextView>(R.id.textViewLogout)
+
+        return view
     }
 }
